51d2001101917b7781e633abbf39a98f45649a9f
northdigital <giotis@email.com>
2017-11-06 18:43:01 +0200

    commit

--- src/gr/northdigital/gdprmanager/Main.java
+++ src/gr/northdigital/gdprmanager/Main.java
@@ -1,5 +1,9 @@
 package gr.northdigital.gdprmanager;
 
+import gr.logismos.orasqlworker.SqlWorker;
+import gr.logismos.orasqlworker.utils.JdbcConBuilder;
+import gr.northdigital.gdprmanager.fxml.MainController;
+import gr.northdigital.gdprmanager.utils.OraHelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,13 +13,32 @@ import javafx.stage.Stage;
 import java.sql.SQLException;
 
 public class Main extends Application {
+  private JdbcConBuilder jdbcConBuilder;
+  SqlWorker sqlWorker;
+
+  public Main() throws SQLException {
+    jdbcConBuilder = new JdbcConBuilder("localhost", "casino", "system", "sporades");
+    sqlWorker = new SqlWorker(jdbcConBuilder);
+  }
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("fxml\\Main.fxml"));
-    primaryStage.setTitle("GDPR Manager");
-    primaryStage.setScene(new Scene(root, 600, 800));
-    primaryStage.show();
+  public void start(Stage primaryStage) {
+
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("fxml\\Main.fxml"));
+
+      primaryStage.setTitle("GDPR Manager");
+      primaryStage.setScene(new Scene(root, 600, 800));
+      primaryStage.show();
+
+      sqlWorker.run(connection -> {
+        for(String user : OraHelper.getOraUsers(connection)) {
+          MainController.mainController.cbUsers.getItems().add(user);
+        }
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) throws SQLException {
--- src/gr/northdigital/gdprmanager/fxml/MainController.java
+++ src/gr/northdigital/gdprmanager/fxml/MainController.java
@@ -1,4 +1,15 @@
 package gr.northdigital.gdprmanager.fxml;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class MainController {
+  public static MainController mainController;
+
+  public MainController() {
+    mainController = this;
+  }
+
+  @FXML
+  public ComboBox<String> cbUsers;
 }

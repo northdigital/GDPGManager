af5b03757cf1d7f8671e0f98b5b7180917057d58
northdigital <giotis@email.com>
2017-11-07 11:23:43 +0200

    commit

--- src/gr/northdigital/gdprmanager/Main.java
+++ src/gr/northdigital/gdprmanager/Main.java
@@ -2,8 +2,6 @@ package gr.northdigital.gdprmanager;
 
 import gr.logismos.orasqlworker.SqlWorker;
 import gr.logismos.orasqlworker.utils.JdbcConBuilder;
-import gr.northdigital.gdprmanager.fxml.MainController;
-import gr.northdigital.gdprmanager.utils.OraHelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
@@ -11,19 +9,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main extends Application {
-  private JdbcConBuilder jdbcConBuilder;
-  SqlWorker sqlWorker;
 
   public Main() throws SQLException {
-    //jdbcConBuilder = new JdbcConBuilder("localhost", "casino", "system", "sporades");
-    jdbcConBuilder = new JdbcConBuilder("192.168.1.201", "casino", "system", "sporades");
-    sqlWorker = new SqlWorker(jdbcConBuilder);
   }
 
   @Override

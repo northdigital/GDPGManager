64166682e8c0db2928d9515d322b7abcfc2512fa
panagiotis <giotismail@yahoo.com>
2017-11-08 11:05:38 +0200

    commit

--- src/gr/northdigital/gdprmanager/fxml/MainController.java
+++ src/gr/northdigital/gdprmanager/fxml/MainController.java
@@ -3,22 +3,23 @@ package gr.northdigital.gdprmanager.fxml;
 import gr.logismos.orasqlworker.SqlWorker;
 import gr.logismos.orasqlworker.utils.JdbcConBuilder;
 import gr.northdigital.gdprmanager.utils.OraHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
   private JdbcConBuilder jdbcConBuilder;
   private SqlWorker sqlWorker;
-
-  public MainController() throws SQLException {
-    //jdbcConBuilder = new JdbcConBuilder("192.168.1.201", "casino", "system", "sporades");
-    // jdbcConBuilder = new JdbcConBuilder("192.168.1.202", "casino", "system", "sporades");
-    jdbcConBuilder = new JdbcConBuilder("localhost", "casino","system", "sporades");
-    sqlWorker = new SqlWorker(jdbcConBuilder);
-  }
+  private ObservableList<String> users;
+  private ObservableList<String> tables;
 
   @FXML
   public ComboBox<String> cbUsers;
@@ -26,25 +27,41 @@ public class MainController {
   @FXML
   public ListView<String> lstTables;
 
-  public void initialize() throws Exception {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    try {
+      jdbcConBuilder = new JdbcConBuilder("192.168.1.201", "casino", "system", "sporades");
+      //jdbcConBuilder = new JdbcConBuilder("192.168.1.202", "casino", "system", "sporades");
+      //jdbcConBuilder = new JdbcConBuilder("localhost", "casino","system", "sporades");
+      sqlWorker = new SqlWorker(jdbcConBuilder);
+
+      users = FXCollections.observableArrayList();
+      tables = FXCollections.observableArrayList();
+
+      cbUsers.setItems(users);
+      lstTables.setItems(tables);
+
+      sqlWorker.run(connection -> {
+        users.clear();
+        users.addAll(OraHelper.getOraUsers(connection));
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public void onShown() throws Exception {
-    sqlWorker.run(connection -> {
-      cbUsers.getItems().addAll(OraHelper.getOraUsers(connection));
-    });
-
-    //cbUsers.getSelectionModel().selectFirst();
-    cbUsers.getSelectionModel().select("CASINOCRM");
+    cbUsers.getSelectionModel().selectFirst();
   }
 
   @FXML
-  public void onAction() throws Exception {
-    String selectedUser = cbUsers.getValue();
-    lstTables.getItems().clear();
-
-    sqlWorker.run(connection -> {
-      lstTables.getItems().addAll(OraHelper.getUserTables(connection, selectedUser));
-    });
+  public void onAction(ActionEvent actionEvent) throws Exception {
+    if (actionEvent.getTarget() == cbUsers) {
+      String selectedUser = cbUsers.getValue();
+      tables.clear();
+      sqlWorker.run(connection -> {
+        tables.addAll(OraHelper.getUserTables(connection, selectedUser));
+      });
+    }
   }
 }

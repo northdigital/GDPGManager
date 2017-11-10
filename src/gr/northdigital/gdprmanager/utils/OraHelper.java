dd99c2dbd501911de9a8555a3ae313e5e889f894
panagiotis <giotismail@yahoo.com>
2017-11-10 11:40:17 +0200

    commit

--- src/gr/northdigital/gdprmanager/utils/OraHelper.java
+++ src/gr/northdigital/gdprmanager/utils/OraHelper.java
@@ -11,13 +11,12 @@ import java.util.List;
 
 public class OraHelper {
   public static List<String> getOraUsers(Connection connection) throws Exception {
-    String sql = "SELECT * FROM DBA_USERS\n" +
-                 "WHERE USERNAME NOT IN (\n" +
-                 "'SYS', 'SYSTEM', 'OUTLN', 'DBSNMP', 'APPQOSSYS', 'XDB', 'GSMADMIN_INTERNAL', 'WMSYS', 'OJVMSYS', 'CTXSYS',\n" +
-                 "'ORDSYS', 'ORDDATA', 'MDSYS', 'LBACSYS', 'OLAPSYS', 'FLOWS_FILES', 'APEX_040200', 'DVSYS', 'HR', 'IX', 'AUDSYS', 'PM',\n" +
-                 "'OE', 'SCOTT', 'ORACLE_OCM', 'XS$NULL', 'MDDATA', 'SYSBACKUP', 'SH', 'DIP', 'SYSDG', 'APEX_PUBLIC_USER',\n" +
-                 "'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'SI_INFORMTN_SCHEMA', 'DVF', 'SYSKM', 'ANONYMOUS', 'ORDPLUGINS'\n" +
-                 ")";
+    String sql = "SELECT username FROM DBA_USERS " +
+                 "WHERE username NOT IN (" +
+                 "        'SYS', 'SYSTEM', 'OUTLN', 'DBSNMP', 'APPQOSSYS', 'XDB', 'GSMADMIN_INTERNAL', 'WMSYS', 'OJVMSYS', 'CTXSYS'," +
+                 "        'ORDSYS', 'ORDDATA', 'MDSYS', 'LBACSYS', 'OLAPSYS', 'FLOWS_FILES', 'APEX_040200', 'DVSYS', 'HR', 'IX', 'AUDSYS', 'PM'," +
+                 "        'OE', 'SCOTT', 'ORACLE_OCM', 'XS$NULL', 'MDDATA', 'SYSBACKUP', 'SH', 'DIP', 'SYSDG', 'APEX_PUBLIC_USER'," +
+                 "        'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'SI_INFORMTN_SCHEMA', 'DVF', 'SYSKM', 'ANONYMOUS', 'ORDPLUGINS')";
     Command command = new Command(connection, sql);
     ArrayList<String> retVal = command.executeSimpleList(String.class);
     command.close();
@@ -26,12 +25,10 @@ public class OraHelper {
   }
 
   public static List<TableDef> getOraTables(Connection connection) throws Exception {
-    String sql = "SELECT OWNER, TABLE_NAME FROM dba_tables\n" +
-                 "WHERE OWNER NOT IN (\n" +
-                 "'SYS', 'SYSTEM', 'OUTLN', 'DBSNMP', 'APPQOSSYS', 'XDB', 'GSMADMIN_INTERNAL', 'WMSYS', 'OJVMSYS', " +
-                 "'CTXSYS', 'ORDSYS', 'ORDDATA', 'MDSYS', 'LBACSYS', 'OLAPSYS', 'FLOWS_FILES', 'APEX_040200', 'DVSYS', " +
-                 "'HR', 'IX', 'AUDSYS', 'PM', 'OE'\n" +
-                 ")";
+    String sql = "SELECT owner, table_name FROM dba_tables " +
+                 "WHERE owner NOT IN (" +
+                 "        'SYS', 'SYSTEM', 'OUTLN', 'DBSNMP', 'APPQOSSYS', 'XDB', 'GSMADMIN_INTERNAL', 'WMSYS', 'OJVMSYS', 'CTXSYS', 'ORDSYS'," +
+                 "        'ORDDATA', 'MDSYS', 'LBACSYS', 'OLAPSYS', 'FLOWS_FILES', 'APEX_040200', 'DVSYS', 'HR', 'IX', 'AUDSYS', 'PM', 'OE')" ;
     Command command = new Command(connection, sql);
     ArrayList<TableDef> retVal = command.executeList(TableDef.class);
     command.close();
@@ -40,15 +37,14 @@ public class OraHelper {
   }
 
   public static List<ColumnDef> getOraColumns(Connection connection) throws Exception {
-    String sql = "SELECT t.owner, t.table_name, t.column_name, t.data_type FROM all_tab_cols t\n" +
-                 "WHERE t.owner NOT IN (\n" +
-                 "  'SYS', 'SYSTEM', 'OUTLN', 'DBSNMP', 'APPQOSSYS', 'XDB', 'GSMADMIN_INTERNAL', 'WMSYS', 'OJVMSYS', 'CTXSYS',\n" +
-                 "  'ORDSYS', 'ORDDATA', 'MDSYS', 'LBACSYS', 'OLAPSYS', 'FLOWS_FILES', 'APEX_040200', 'DVSYS', 'HR', 'IX', 'AUDSYS', 'PM',\n" +
-                 "  'OE', 'SCOTT', 'ORACLE_OCM', 'XS$NULL', 'MDDATA', 'SYSBACKUP', 'SH', 'DIP', 'SYSDG', 'APEX_PUBLIC_USER',\n" +
-                 "  'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'SI_INFORMTN_SCHEMA', 'DVF', 'SYSKM', 'ANONYMOUS', 'ORDPLUGINS'\n" +
-                 ") AND NOT EXISTS (\n" +
-                 "  SELECT 0 FROM all_views t2 WHERE t2.owner = t.owner AND t2.view_name = t.table_name\n" +
-                 ")";
+    String sql = "SELECT t.owner, t.table_name, t.column_name, t.data_type FROM all_tab_cols t " +
+                 "WHERE t.owner NOT IN (" +
+                 "        'SYS', 'SYSTEM', 'OUTLN', 'DBSNMP', 'APPQOSSYS', 'XDB', 'GSMADMIN_INTERNAL', 'WMSYS', 'OJVMSYS', 'CTXSYS'," +
+                 "        'ORDSYS', 'ORDDATA', 'MDSYS', 'LBACSYS', 'OLAPSYS', 'FLOWS_FILES', 'APEX_040200', 'DVSYS', 'HR', 'IX', 'AUDSYS', 'PM'," +
+                 "        'OE', 'SCOTT', 'ORACLE_OCM', 'XS$NULL', 'MDDATA', 'SYSBACKUP', 'SH', 'DIP', 'SYSDG', 'APEX_PUBLIC_USER'," +
+                 "        'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'SI_INFORMTN_SCHEMA', 'DVF', 'SYSKM', 'ANONYMOUS', 'ORDPLUGINS')" +
+                 "      AND NOT EXISTS (" +
+                 "        SELECT 0 FROM all_views t2 WHERE t2.owner = t.owner AND t2.view_name = t.table_name)";
     Command command = new Command(connection, sql);
     ArrayList<ColumnDef> retVal = command.executeList(ColumnDef.class);
     command.close();
